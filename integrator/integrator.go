8649837cd8553c607e169b1aaf2a4444b38bc2f5
mrpk1906 <mrpk1906@gmail.com>
2023-06-01 09:17:59 +0700

    refactor: fix issue couldn't parse long lines

--- integrator/integrator.go
+++ integrator/integrator.go
@@ -1,12 +1,13 @@
 package integrator
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/admicrovn/wazuh-integratord/config"
 	"github.com/gammazero/workerpool"
-	"github.com/masa23/gotail"
+	"github.com/goccy/go-json"
+	"github.com/grafana/tail"
 	log "github.com/sirupsen/logrus"
+	"io"
 	"math/rand"
 	"os"
 	"os/exec"
@@ -16,18 +17,19 @@ import (
 )
 
 const (
-	defaultAlertsJsonPath   = "/var/ossec/logs/alerts/alerts.json"
+	defaultAlertsJSONPath   = "/var/ossec/logs/alerts/alerts.json"
 	defaultIntegrationsPath = "/var/ossec/integrations"
+	DEV                     = "dev"
 )
 
-type Event struct {
+type Alert struct {
 	Rule     Rule   `json:"rule,omitempty"`
 	Location string `json:"location,omitempty"`
 }
 
 type Rule struct {
 	Level  *int     `json:"level,omitempty"`
-	Id     string   `json:"id"`
+	ID     string   `json:"id"`
 	Groups []string `json:"groups,omitempty"`
 }
 
@@ -51,143 +53,37 @@ func (i *Integrator) Run() {
 	for _, integration := range i.config.WazuhConfig.Integrations {
 		log.Infof("enabling integration for: '%s'", integration.Name)
 	}
-	alertsPath := defaultAlertsJsonPath
+	alertsJSONPath := defaultAlertsJSONPath
 	// development
-	if os.Getenv("ENV") == "dev" {
-		alertsPath = "./alerts.json"
+	if os.Getenv("ENV") == DEV {
+		alertsJSONPath = "./alerts.json"
 	}
-	tail, err := gotail.Open(alertsPath, "")
+
+	t, err := tail.TailFile(alertsJSONPath, tail.Config{
+		Logger: log.StandardLogger(),
+		Follow: true,
+		ReOpen: true,
+		Poll:   true,
+		Location: &tail.SeekInfo{
+			Whence: io.SeekEnd,
+		},
+	})
 	if err != nil {
-		log.Panic(err)
+		log.Fatalf("failed to tail file: %s", err.Error())
 	}
-	// read from the end of file
-	tail.InitialReadPositionEnd = true
+
+	ch := make(chan string)
+	go i.handleAlert(ch)
 
 	var fullEvent string
-	retries := 0
-	for tail.Scan() {
-		line := tail.Text()
-		if len(line) > 0 {
-			lastChar := line[len(line)-1:]
-			if lastChar != "}" {
-				fullEvent = fullEvent + line
-				continue
-			} else {
-				if len(fullEvent) > 0 {
-					fullEvent = fullEvent + line
-				} else {
-					fullEvent = line
-				}
-			}
-		}
-		log.Debug("sending new alert")
-		var event Event
-		eventData := []byte(fullEvent)
-		err = json.Unmarshal(eventData, &event)
-		if err != nil {
-			if retries == 0 {
-				retries++
-				continue
-			}
-			log.Errorf("unmarshal event err: %s, raw: %s", err, fullEvent)
-			// truncate fullEvent
-			fullEvent = ""
-			// reset retries
-			retries = 0
+	for line := range t.Lines {
+		text := line.Text
+		if strings.HasPrefix(text, "{\"timestamp\"") && strings.HasSuffix(text, "}") {
+			ch <- text
 			continue
 		}
-		// truncate fullEvent
-		fullEvent = ""
-		// reset retries
-		retries = 0
-		if len(event.Rule.Id) == 0 {
-			log.Trace("skipping: alert doesn't contain rule id")
-			continue
-		}
-		i.wp.Submit(func() {
-			for _, integration := range i.config.WazuhConfig.Integrations {
-				if len(event.Location) == 0 {
-					log.Tracef("[%s] skipping: alert doesn't contain location", integration.Name)
-					continue
-				}
-				if len(integration.EventLocations) > 0 {
-					var match bool
-					for _, eventLocation := range integration.EventLocations {
-						if strings.Contains(event.Location, eventLocation) {
-							match = true
-							break
-						}
-					}
-					if !match {
-						log.Tracef("[%s] skipping: location doesn't match", integration.Name)
-						continue
-					}
-				}
-				if integration.Level != nil {
-					if event.Rule.Level == nil {
-						log.Tracef("[%s] skipping: alert doesn't contain rule level", integration.Name)
-						continue
-					}
-					if *event.Rule.Level < *integration.Level {
-						log.Tracef("[%s] skipping: alert level is too low", integration.Name)
-						continue
-					}
-				}
-				if len(integration.Groups) > 0 {
-					var match bool
-					for _, integrationGroup := range integration.Groups {
-						for _, ruleGroup := range event.Rule.Groups {
-							if integrationGroup == ruleGroup {
-								match = true
-								break
-							}
-						}
-						if match {
-							break
-						}
-					}
-					if !match {
-						log.Tracef("[%s] skipping: group doesn't match", integration.Name)
-						continue
-					}
-				}
-				if len(integration.RuleIDs) > 0 {
-					var ruleID int
-					ruleID, err = strconv.Atoi(event.Rule.Id)
-					if err != nil {
-						log.Errorf("[%s] alert rule id must be a number", integration.Name)
-						continue
-					}
-					if !contains(integration.RuleIDs, ruleID) {
-						log.Tracef("[%s] skipping: rule id doesn't match", integration.Name)
-						continue
-					}
-				}
-				var tmpFile string
-				tmpFile, err = createTmpAlertFile(integration.Name, eventData)
-				if err != nil {
-					log.Errorf("[%s] create tmp file err: %s", integration.Name, err)
-					goto CleanUp
-				}
-				// execute integration command
-				err = executeCommand(integration.Name, tmpFile, *integration.ApiKey, *integration.HookUrl)
-				if err != nil {
-					log.Errorf("[%s] exec integration command err: %s", integration.Name, err)
-				}
-				goto CleanUp
-			CleanUp:
-				// remove tmp file
-				err = os.Remove(tmpFile)
-				if err != nil {
-					log.Errorf("[%s] remove tmp file err: %s", integration.Name, err)
-					continue
-				}
-			}
-		})
-	}
-
-	if err = tail.Err(); err != nil {
-		log.Fatalf("Tail %s err: %s", alertsPath, err)
+		fullEvent += text
+		continue
 	}
 }
 
@@ -203,10 +99,9 @@ func (i *Integrator) Stats() {
 // createTmpAlertFile create temp file for alert
 func createTmpAlertFile(integrationName string, data []byte) (string, error) {
 	now := time.Now().Unix()
-	rand.Seed(time.Now().UnixNano())
-	randNum := randomInt(100000000, 999999999)
+	randNum := rand.Int() //nolint:gosec
 	tmpFile := fmt.Sprintf("/tmp/%s-%d-%d.alert", integrationName, now, randNum)
-	err := os.WriteFile(tmpFile, data, 0644)
+	err := os.WriteFile(tmpFile, data, 0644) //nolint:gosec
 	if err != nil {
 		return "", err
 	}
@@ -214,24 +109,20 @@ func createTmpAlertFile(integrationName string, data []byte) (string, error) {
 }
 
 // executeCommand execute integration command
-func executeCommand(integrationName, tempFile, apiKey, hookUrl string) error {
+func executeCommand(integrationName, tempFile, apiKey, hookURL string) error {
 	integrationBin := fmt.Sprintf("%s/%s", defaultIntegrationsPath, integrationName)
 	// development
-	if os.Getenv("ENV") == "dev" {
+	if os.Getenv("ENV") == DEV {
 		integrationBin = "./custom-integration.sh"
 	}
-	integrationCmd := exec.Command(integrationBin, tempFile, apiKey, hookUrl)
+	integrationCmd := exec.Command(integrationBin, tempFile, apiKey, hookURL)
 	log.Debugf("running: %s", integrationCmd.String())
 	out, err := integrationCmd.CombinedOutput()
 	if err != nil {
-		return fmt.Errorf("err: %s, out: %s", err, out)
+		return fmt.Errorf("err: %w, out: %s", err, out)
 	}
 	log.Debugf("[%s] command ran successfully", integrationName)
 	return nil
-}
-
-func randomInt(min, max int) int {
-	return min + rand.Intn(max-min)
 }
 
 func contains(arr []int, i int) bool {
@@ -241,4 +132,105 @@ func contains(arr []int, i int) bool {
 		}
 	}
 	return false
+}
+
+// handleAlert parse alert json
+func (i *Integrator) handleAlert(c chan string) {
+	for {
+		s := <-c
+		log.Debug("sending new alert")
+		var alert Alert
+		err := json.Unmarshal([]byte(s), &alert)
+		if err != nil {
+			log.Errorf("unmarshal event err: %s, raw: %s", err, s)
+			continue
+		}
+		if len(alert.Rule.ID) == 0 {
+			log.Trace("skipping: alert doesn't contain rule id")
+			continue
+		}
+		log.Trace(s)
+		i.parseAlertAndRunIntegration(s, alert)
+	}
+}
+
+func (i *Integrator) parseAlertAndRunIntegration(s string, alert Alert) {
+	i.wp.Submit(func() {
+		for _, integration := range i.config.WazuhConfig.Integrations {
+			if len(alert.Location) == 0 {
+				log.Tracef("[%s] skipping: alert doesn't contain location", integration.Name)
+				continue
+			}
+			if len(integration.EventLocations) > 0 {
+				var match bool
+				for _, eventLocation := range integration.EventLocations {
+					if strings.Contains(alert.Location, eventLocation) {
+						match = true
+						break
+					}
+				}
+				if !match {
+					log.Tracef("[%s] skipping: location doesn't match", integration.Name)
+					continue
+				}
+			}
+			if integration.Level != nil {
+				if alert.Rule.Level == nil {
+					log.Tracef("[%s] skipping: alert doesn't contain rule level", integration.Name)
+					continue
+				}
+				if *alert.Rule.Level < *integration.Level {
+					log.Tracef("[%s] skipping: alert level is too low", integration.Name)
+					continue
+				}
+			}
+			if len(integration.Groups) > 0 {
+				var match bool
+				for _, integrationGroup := range integration.Groups {
+					for _, ruleGroup := range alert.Rule.Groups {
+						if integrationGroup == ruleGroup {
+							match = true
+							break
+						}
+					}
+					if match {
+						break
+					}
+				}
+				if !match {
+					log.Tracef("[%s] skipping: group doesn't match", integration.Name)
+					continue
+				}
+			}
+			if len(integration.RuleIDs) > 0 {
+				ruleID, err := strconv.Atoi(alert.Rule.ID)
+				if err != nil {
+					log.Errorf("[%s] alert rule id must be a number", integration.Name)
+					continue
+				}
+				if !contains(integration.RuleIDs, ruleID) {
+					log.Tracef("[%s] skipping: rule id doesn't match", integration.Name)
+					continue
+				}
+			}
+			tmpFile, err := createTmpAlertFile(integration.Name, []byte(s))
+			if err != nil {
+				log.Errorf("[%s] create tmp file err: %s", integration.Name, err)
+				goto CleanUp
+			}
+			// execute integration command
+			err = executeCommand(integration.Name, tmpFile, *integration.ApiKey, *integration.HookUrl)
+			if err != nil {
+				log.Errorf("[%s] exec integration command err: %s", integration.Name, err)
+			}
+			goto CleanUp
+		CleanUp:
+			// remove tmp file
+			err = os.Remove(tmpFile)
+			if err != nil {
+				log.Errorf("[%s] remove tmp file err: %s", integration.Name, err)
+				continue
+			}
+		}
+	})
 }
